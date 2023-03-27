ddc0f4b99cd73f8c7f9d1df9e72497489c0a7b8b
ppp <ppp.3ppj@gmail.com>
2023-03-27 10:20:13 +0700

    Binding Screen

--- AndroidBindingByJavaLib/AndroidBindingByJavaLib/obj/Debug/net7.0-android/generated/src/Java.Interop.__TypeRegistrations.cs
+++ AndroidBindingByJavaLib/AndroidBindingByJavaLib/obj/Debug/net7.0-android/generated/src/Java.Interop.__TypeRegistrations.cs
@@ -14,8 +14,12 @@ namespace Java.Interop {
 #endif // def MONODROID_TIMING
 			Java.Interop.TypeManager.RegisterPackages (
 					new string[]{
+						"com/sunmi/aidl",
+						"org/greenrobot/greendao/query",
 					},
 					new Converter<string, Type?>[]{
+						lookup_com_sunmi_aidl_package,
+						lookup_org_greenrobot_greendao_query_package,
 					});
 #if MONODROID_TIMING
 			var end = DateTime.Now;
@@ -32,6 +36,33 @@ namespace Java.Interop {
 			if (managedType == null)
 				return null;
 			return Type.GetType (managedType);
+		}
+
+		static string[]? package_com_sunmi_aidl_mappings;
+		static Type? lookup_com_sunmi_aidl_package (string klass)
+		{
+			if (package_com_sunmi_aidl_mappings == null) {
+				package_com_sunmi_aidl_mappings = new string[]{
+					"com/sunmi/aidl/SendService$Stub:Com.Sunmi.Aidl.ISendService/Stub",
+					"com/sunmi/aidl/SendServiceCallback$Stub:Com.Sunmi.Aidl.ISendServiceCallback/Stub",
+				};
+			}
+
+			return Lookup (package_com_sunmi_aidl_mappings, klass);
+		}
+
+		static string[]? package_org_greenrobot_greendao_query_mappings;
+		static Type? lookup_org_greenrobot_greendao_query_package (string klass)
+		{
+			if (package_org_greenrobot_greendao_query_mappings == null) {
+				package_org_greenrobot_greendao_query_mappings = new string[]{
+					"org/greenrobot/greendao/query/WhereCondition$AbstractCondition:Org.Greenrobot.Greendao.Query.IWhereCondition/AbstractCondition",
+					"org/greenrobot/greendao/query/WhereCondition$PropertyCondition:Org.Greenrobot.Greendao.Query.IWhereCondition/PropertyCondition",
+					"org/greenrobot/greendao/query/WhereCondition$StringCondition:Org.Greenrobot.Greendao.Query.IWhereCondition/StringCondition",
+				};
+			}
+
+			return Lookup (package_org_greenrobot_greendao_query_mappings, klass);
 		}
 	}
 }
--- AndroidBindingByJavaLib/AndroidBindingByJavaLib/obj/Debug/net7.0-android/generated/src/__NamespaceMapping__.cs
+++ AndroidBindingByJavaLib/AndroidBindingByJavaLib/obj/Debug/net7.0-android/generated/src/__NamespaceMapping__.cs
@@ -1,5 +1,15 @@
 using System;
 
+[assembly:global::Android.Runtime.NamespaceMapping (Java = "com.alibaba.fastjson", Managed="Com.Alibaba.Fastjson")]
+[assembly:global::Android.Runtime.NamespaceMapping (Java = "com.alibaba.fastjson.annotation", Managed="Com.Alibaba.Fastjson.Annotation")]
+[assembly:global::Android.Runtime.NamespaceMapping (Java = "com.alibaba.fastjson.parser", Managed="Com.Alibaba.Fastjson.Parser")]
+[assembly:global::Android.Runtime.NamespaceMapping (Java = "com.alibaba.fastjson.serializer", Managed="Com.Alibaba.Fastjson.Serializer")]
+[assembly:global::Android.Runtime.NamespaceMapping (Java = "com.alibaba.fastjson.spi", Managed="Com.Alibaba.Fastjson.Spi")]
+[assembly:global::Android.Runtime.NamespaceMapping (Java = "com.alibaba.fastjson.support.config", Managed="Com.Alibaba.Fastjson.Support.Config")]
+[assembly:global::Android.Runtime.NamespaceMapping (Java = "com.alibaba.fastjson.support.geo", Managed="Com.Alibaba.Fastjson.Support.Geo")]
+[assembly:global::Android.Runtime.NamespaceMapping (Java = "com.alibaba.fastjson.support.hsf", Managed="Com.Alibaba.Fastjson.Support.Hsf")]
+[assembly:global::Android.Runtime.NamespaceMapping (Java = "com.alibaba.fastjson.support.spring", Managed="Com.Alibaba.Fastjson.Support.Spring")]
+[assembly:global::Android.Runtime.NamespaceMapping (Java = "com.alibaba.fastjson.util", Managed="Com.Alibaba.Fastjson.Util")]
 [assembly:global::Android.Runtime.NamespaceMapping (Java = "com.google.zxing", Managed="Com.Google.Zxing")]
 [assembly:global::Android.Runtime.NamespaceMapping (Java = "com.google.zxing.aztec", Managed="Com.Google.Zxing.Aztec")]
 [assembly:global::Android.Runtime.NamespaceMapping (Java = "com.google.zxing.aztec.decoder", Managed="Com.Google.Zxing.Aztec.Decoder")]
@@ -33,37 +43,106 @@ using System;
 [assembly:global::Android.Runtime.NamespaceMapping (Java = "com.google.zxing.qrcode.encoder", Managed="Com.Google.Zxing.Qrcode.Encoder")]
 [assembly:global::Android.Runtime.NamespaceMapping (Java = "com.ppp.sunmit2printer", Managed="Com.Ppp.Sunmit2printer")]
 [assembly:global::Android.Runtime.NamespaceMapping (Java = "com.ppp.sunmit2printer.utils", Managed="Com.Ppp.Sunmit2printer.Utils")]
+[assembly:global::Android.Runtime.NamespaceMapping (Java = "com.ppp.sunmit2vicescreen", Managed="Com.Ppp.Sunmit2vicescreen")]
+[assembly:global::Android.Runtime.NamespaceMapping (Java = "com.ppp.sunmit2vicescreen.receiver", Managed="Com.Ppp.Sunmit2vicescreen.Receiver")]
+[assembly:global::Android.Runtime.NamespaceMapping (Java = "com.ppp.sunmit2vicescreen.utils", Managed="Com.Ppp.Sunmit2vicescreen.Utils")]
+[assembly:global::Android.Runtime.NamespaceMapping (Java = "com.ppp.sunmit2vicescreen.utils_old", Managed="Com.Ppp.Sunmit2vicescreen.Utils_old")]
+[assembly:global::Android.Runtime.NamespaceMapping (Java = "com.sunmi.aidl", Managed="Com.Sunmi.Aidl")]
 [assembly:global::Android.Runtime.NamespaceMapping (Java = "com.sunmi.library", Managed="Com.Sunmi.Library")]
 [assembly:global::Android.Runtime.NamespaceMapping (Java = "com.sunmi.peripheral.printer", Managed="Com.Sunmi.Peripheral.Printer")]
+[assembly:global::Android.Runtime.NamespaceMapping (Java = "org.greenrobot.greendao", Managed="Org.Greenrobot.Greendao")]
+[assembly:global::Android.Runtime.NamespaceMapping (Java = "org.greenrobot.greendao.async", Managed="Org.Greenrobot.Greendao.Async")]
+[assembly:global::Android.Runtime.NamespaceMapping (Java = "org.greenrobot.greendao.database", Managed="Org.Greenrobot.Greendao.Database")]
+[assembly:global::Android.Runtime.NamespaceMapping (Java = "org.greenrobot.greendao.identityscope", Managed="Org.Greenrobot.Greendao.Identityscope")]
+[assembly:global::Android.Runtime.NamespaceMapping (Java = "org.greenrobot.greendao.internal", Managed="Org.Greenrobot.Greendao.Internal")]
+[assembly:global::Android.Runtime.NamespaceMapping (Java = "org.greenrobot.greendao.query", Managed="Org.Greenrobot.Greendao.Query")]
+[assembly:global::Android.Runtime.NamespaceMapping (Java = "org.greenrobot.greendao.rx", Managed="Org.Greenrobot.Greendao.RX")]
+[assembly:global::Android.Runtime.NamespaceMapping (Java = "sunmi.ds", Managed="Sunmi.DS")]
+[assembly:global::Android.Runtime.NamespaceMapping (Java = "sunmi.ds.callback", Managed="Sunmi.DS.Callback")]
+[assembly:global::Android.Runtime.NamespaceMapping (Java = "sunmi.ds.data", Managed="Sunmi.DS.Data")]
+[assembly:global::Android.Runtime.NamespaceMapping (Java = "sunmi.ds.exception", Managed="Sunmi.DS.Exception")]
+[assembly:global::Android.Runtime.NamespaceMapping (Java = "sunmi.ds.gen", Managed="Sunmi.DS.Gen")]
+[assembly:global::Android.Runtime.NamespaceMapping (Java = "sunmi.ds.utils", Managed="Sunmi.DS.Utils")]
 
 delegate sbyte _JniMarshal_PP_B (IntPtr jnienv, IntPtr klass);
+delegate char _JniMarshal_PP_C (IntPtr jnienv, IntPtr klass);
+delegate double _JniMarshal_PP_D (IntPtr jnienv, IntPtr klass);
 delegate int _JniMarshal_PP_I (IntPtr jnienv, IntPtr klass);
+delegate long _JniMarshal_PP_J (IntPtr jnienv, IntPtr klass);
 delegate IntPtr _JniMarshal_PP_L (IntPtr jnienv, IntPtr klass);
 delegate void _JniMarshal_PP_V (IntPtr jnienv, IntPtr klass);
 delegate bool _JniMarshal_PP_Z (IntPtr jnienv, IntPtr klass);
 delegate void _JniMarshal_PPB_V (IntPtr jnienv, IntPtr klass, sbyte p0);
+delegate IntPtr _JniMarshal_PPC_L (IntPtr jnienv, IntPtr klass, char p0);
+delegate void _JniMarshal_PPC_V (IntPtr jnienv, IntPtr klass, char p0);
+delegate void _JniMarshal_PPD_V (IntPtr jnienv, IntPtr klass, double p0);
+delegate sbyte _JniMarshal_PPI_B (IntPtr jnienv, IntPtr klass, int p0);
+delegate double _JniMarshal_PPI_D (IntPtr jnienv, IntPtr klass, int p0);
+delegate float _JniMarshal_PPI_F (IntPtr jnienv, IntPtr klass, int p0);
 delegate int _JniMarshal_PPI_I (IntPtr jnienv, IntPtr klass, int p0);
+delegate long _JniMarshal_PPI_J (IntPtr jnienv, IntPtr klass, int p0);
+delegate IntPtr _JniMarshal_PPI_L (IntPtr jnienv, IntPtr klass, int p0);
+delegate short _JniMarshal_PPI_S (IntPtr jnienv, IntPtr klass, int p0);
 delegate void _JniMarshal_PPI_V (IntPtr jnienv, IntPtr klass, int p0);
+delegate bool _JniMarshal_PPI_Z (IntPtr jnienv, IntPtr klass, int p0);
+delegate void _JniMarshal_PPID_V (IntPtr jnienv, IntPtr klass, int p0, double p1);
+delegate IntPtr _JniMarshal_PPII_L (IntPtr jnienv, IntPtr klass, int p0, int p1);
 delegate IntPtr _JniMarshal_PPIIII_L (IntPtr jnienv, IntPtr klass, int p0, int p1, int p2, int p3);
+delegate void _JniMarshal_PPIIL_V (IntPtr jnienv, IntPtr klass, int p0, int p1, IntPtr p2);
+delegate void _JniMarshal_PPIJ_V (IntPtr jnienv, IntPtr klass, int p0, long p1);
+delegate void _JniMarshal_PPIJJ_V (IntPtr jnienv, IntPtr klass, int p0, long p1, long p2);
 delegate IntPtr _JniMarshal_PPIL_L (IntPtr jnienv, IntPtr klass, int p0, IntPtr p1);
+delegate void _JniMarshal_PPIL_V (IntPtr jnienv, IntPtr klass, int p0, IntPtr p1);
+delegate bool _JniMarshal_PPIL_Z (IntPtr jnienv, IntPtr klass, int p0, IntPtr p1);
 delegate IntPtr _JniMarshal_PPILL_L (IntPtr jnienv, IntPtr klass, int p0, IntPtr p1, IntPtr p2);
+delegate bool _JniMarshal_PPILLI_Z (IntPtr jnienv, IntPtr klass, int p0, IntPtr p1, IntPtr p2, int p3);
 delegate IntPtr _JniMarshal_PPILLL_L (IntPtr jnienv, IntPtr klass, int p0, IntPtr p1, IntPtr p2, IntPtr p3);
+delegate IntPtr _JniMarshal_PPJ_L (IntPtr jnienv, IntPtr klass, long p0);
+delegate void _JniMarshal_PPJ_V (IntPtr jnienv, IntPtr klass, long p0);
+delegate bool _JniMarshal_PPJ_Z (IntPtr jnienv, IntPtr klass, long p0);
+delegate void _JniMarshal_PPJJ_V (IntPtr jnienv, IntPtr klass, long p0, long p1);
+delegate void _JniMarshal_PPJL_V (IntPtr jnienv, IntPtr klass, long p0, IntPtr p1);
+delegate long _JniMarshal_PPJLLL_J (IntPtr jnienv, IntPtr klass, long p0, IntPtr p1, IntPtr p2, IntPtr p3);
+delegate int _JniMarshal_PPL_I (IntPtr jnienv, IntPtr klass, IntPtr p0);
+delegate long _JniMarshal_PPL_J (IntPtr jnienv, IntPtr klass, IntPtr p0);
 delegate IntPtr _JniMarshal_PPL_L (IntPtr jnienv, IntPtr klass, IntPtr p0);
 delegate void _JniMarshal_PPL_V (IntPtr jnienv, IntPtr klass, IntPtr p0);
+delegate bool _JniMarshal_PPL_Z (IntPtr jnienv, IntPtr klass, IntPtr p0);
 delegate void _JniMarshal_PPLFZZL_V (IntPtr jnienv, IntPtr klass, IntPtr p0, float p1, bool p2, bool p3, IntPtr p4);
+delegate IntPtr _JniMarshal_PPLI_L (IntPtr jnienv, IntPtr klass, IntPtr p0, int p1);
 delegate void _JniMarshal_PPLI_V (IntPtr jnienv, IntPtr klass, IntPtr p0, int p1);
 delegate void _JniMarshal_PPLII_V (IntPtr jnienv, IntPtr klass, IntPtr p0, int p1, int p2);
 delegate IntPtr _JniMarshal_PPLIIFFFFFFFFFFFFFFFF_L (IntPtr jnienv, IntPtr klass, IntPtr p0, int p1, int p2, float p3, float p4, float p5, float p6, float p7, float p8, float p9, float p10, float p11, float p12, float p13, float p14, float p15, float p16, float p17, float p18);
 delegate void _JniMarshal_PPLIIII_V (IntPtr jnienv, IntPtr klass, IntPtr p0, int p1, int p2, int p3, int p4);
 delegate IntPtr _JniMarshal_PPLIIL_L (IntPtr jnienv, IntPtr klass, IntPtr p0, int p1, int p2, IntPtr p3);
+delegate IntPtr _JniMarshal_PPLIL_L (IntPtr jnienv, IntPtr klass, IntPtr p0, int p1, IntPtr p2);
+delegate void _JniMarshal_PPLIL_V (IntPtr jnienv, IntPtr klass, IntPtr p0, int p1, IntPtr p2);
+delegate IntPtr _JniMarshal_PPLJ_L (IntPtr jnienv, IntPtr klass, IntPtr p0, long p1);
+delegate void _JniMarshal_PPLJ_V (IntPtr jnienv, IntPtr klass, IntPtr p0, long p1);
+delegate void _JniMarshal_PPLJJ_V (IntPtr jnienv, IntPtr klass, IntPtr p0, long p1, long p2);
+delegate void _JniMarshal_PPLJZ_V (IntPtr jnienv, IntPtr klass, IntPtr p0, long p1, bool p2);
 delegate IntPtr _JniMarshal_PPLL_L (IntPtr jnienv, IntPtr klass, IntPtr p0, IntPtr p1);
 delegate void _JniMarshal_PPLL_V (IntPtr jnienv, IntPtr klass, IntPtr p0, IntPtr p1);
 delegate bool _JniMarshal_PPLL_Z (IntPtr jnienv, IntPtr klass, IntPtr p0, IntPtr p1);
+delegate IntPtr _JniMarshal_PPLLI_L (IntPtr jnienv, IntPtr klass, IntPtr p0, IntPtr p1, int p2);
+delegate void _JniMarshal_PPLLI_V (IntPtr jnienv, IntPtr klass, IntPtr p0, IntPtr p1, int p2);
 delegate IntPtr _JniMarshal_PPLLII_L (IntPtr jnienv, IntPtr klass, IntPtr p0, IntPtr p1, int p2, int p3);
 delegate IntPtr _JniMarshal_PPLLIIL_L (IntPtr jnienv, IntPtr klass, IntPtr p0, IntPtr p1, int p2, int p3, IntPtr p4);
+delegate void _JniMarshal_PPLLJL_V (IntPtr jnienv, IntPtr klass, IntPtr p0, IntPtr p1, long p2, IntPtr p3);
 delegate int _JniMarshal_PPLLL_I (IntPtr jnienv, IntPtr klass, IntPtr p0, IntPtr p1, IntPtr p2);
+delegate long _JniMarshal_PPLLL_J (IntPtr jnienv, IntPtr klass, IntPtr p0, IntPtr p1, IntPtr p2);
+delegate IntPtr _JniMarshal_PPLLL_L (IntPtr jnienv, IntPtr klass, IntPtr p0, IntPtr p1, IntPtr p2);
 delegate void _JniMarshal_PPLLL_V (IntPtr jnienv, IntPtr klass, IntPtr p0, IntPtr p1, IntPtr p2);
 delegate void _JniMarshal_PPLLLF_V (IntPtr jnienv, IntPtr klass, IntPtr p0, IntPtr p1, IntPtr p2, float p3);
+delegate long _JniMarshal_PPLLLL_J (IntPtr jnienv, IntPtr klass, IntPtr p0, IntPtr p1, IntPtr p2, IntPtr p3);
+delegate IntPtr _JniMarshal_PPLLLL_L (IntPtr jnienv, IntPtr klass, IntPtr p0, IntPtr p1, IntPtr p2, IntPtr p3);
+delegate void _JniMarshal_PPLLLL_V (IntPtr jnienv, IntPtr klass, IntPtr p0, IntPtr p1, IntPtr p2, IntPtr p3);
+delegate void _JniMarshal_PPLLZ_V (IntPtr jnienv, IntPtr klass, IntPtr p0, IntPtr p1, bool p2);
+delegate int _JniMarshal_PPLLZJL_I (IntPtr jnienv, IntPtr klass, IntPtr p0, IntPtr p1, bool p2, long p3, IntPtr p4);
+delegate void _JniMarshal_PPLZ_V (IntPtr jnienv, IntPtr klass, IntPtr p0, bool p1);
+delegate IntPtr _JniMarshal_PPZ_L (IntPtr jnienv, IntPtr klass, bool p0);
+delegate void _JniMarshal_PPZ_V (IntPtr jnienv, IntPtr klass, bool p0);
+delegate bool _JniMarshal_PPZ_Z (IntPtr jnienv, IntPtr klass, bool p0);
 #if !NET
 namespace System.Runtime.Versioning {
     [System.Diagnostics.Conditional("NEVER")]
